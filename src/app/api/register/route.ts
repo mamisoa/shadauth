82ab6643260e3857b234172bad06416fd26da328
mamisoa <mamisoa@gmail.com>
2024-11-23 11:27:10 +0100

    Refactor registration API to improve error handling and streamline user creation process

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,49 +1,58 @@
-import { NextResponse } from 'next/server'
-import { z } from 'zod'
-import { prisma } from '@/prisma/prisma'
-import { saltAndHash } from '@/src/_actions/utils'
+import { NextResponse } from "next/server";
+import { z } from "zod";
+import { prisma } from "@/prisma/prisma";
+import { saltAndHash } from "@/src/_actions/utils";
 
 const registerSchema = z.object({
-  username: z.string().min(1),
-  firstName: z.string().min(1),
-  lastName: z.string().min(1),
-  email: z.string().email().max(300),
-  password: z.string().min(3),
-})
+	username: z.string().min(1),
+	firstName: z.string().min(1),
+	lastName: z.string().min(1),
+	email: z.string().email().max(300),
+	password: z.string().min(3),
+});
 
 export async function POST(req: Request) {
-  try {
-    const body = await req.json()
-    const { username, firstname, lastname, email, password } = registerSchema.parse(body)
+	try {
+		const body = await req.json();
+		const { username, email, password } = registerSchema.parse(body);
 
-    // Check if email is already taken
-    const existingEmail = await prisma.user.findUnique({
-      where: { email },
-    })
+		// Check if email is already taken
+		const existingEmail = await prisma.user.findUnique({
+			where: { email },
+		});
 
-    if (existingEmail) {
-      return NextResponse.json({ error: 'Email already in use' }, { status: 400 })
-    }
+		if (existingEmail) {
+			return NextResponse.json(
+				{ error: "Email already in use" },
+				{ status: 400 }
+			);
+		}
 
-    // Hash the password
-    const hashedPassword = await saltAndHash(password)
+		// Hash the password
+		const hashedPassword = await saltAndHash(password);
 
-    // Create the user
-    await prisma.user.create({
-      data: {
-        username,
-        firstname,
-        lastname,
-        email,
-        password: hashedPassword,
-      },
-    })
+		// Create the user
+		await prisma.user.create({
+			data: {
+				username,
+				firstname,
+				lastname,
+				email,
+				password: hashedPassword,
+			},
+		});
 
-    return NextResponse.json({ message: 'User registered successfully' }, { status: 201 })
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      return NextResponse.json({ error: error.errors }, { status: 400 })
-    }
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
-  }
+		return NextResponse.json(
+			{ message: "User registered successfully" },
+			{ status: 201 }
+		);
+	} catch (error) {
+		if (error instanceof z.ZodError) {
+			return NextResponse.json({ error: error.errors }, { status: 400 });
+		}
+		return NextResponse.json(
+			{ error: "Internal Server Error" },
+			{ status: 500 }
+		);
+	}
 }
